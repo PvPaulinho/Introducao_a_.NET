4651c11a165b99c0a1b702914afae58a23d18375
Paulo Victor <pvictor36@gmail.com>
2024-02-22 21:27:30 -0300

    Criando HelloWorld

---
+++ IntroducaoAoCSharpUndemy/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Pasta Lógica
+namespace HelloWorld
+{
+    /*
+        internal representa que é uma classe interna, não é necessário expressar onde a classe se ingloba,
+        somente quando se desesja expecificar
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imprimir na tela
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Meu nome é Paulo Victor");
+        }
+    }
+}
